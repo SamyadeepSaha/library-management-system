3c5e4cdc689136aad380719bd2f1fe906c8c1ca5
SamyadeepSaha <samyadeep98@gmail.com>
2023-12-19 21:13:37 +0530

    Write issue and return function

--- database.c
+++ database.c
@@ -90,11 +90,40 @@ int del_book(char *id)
 
 int issue_book(char *bk_id, char *m_id)
 {
+    sqlite3 *db;
+    char *err_msg = 0;
+    char *sql = 0;
+    int status = 0;
+    sprintf(sql,"insert into issue values('%s', '%s', date('now'));", bk_id, m_id);
+    if(sqlite3_open(DB, &db) == SQLITE_OK)
+    {
+        if(sqlite3_exec(db, sql, 0, 0, &err_msg) == SQLITE_OK)
+            printf("SQL insertion successful\n");
+        else
+            status = printf("SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+    }
+    return status;
     return 0;
 }
 
 int return_book(char *bk_id, char *m_id)
 {
-    return 0;
+    sqlite3 *db;
+    char *err_msg = 0;
+    char *sql = 0;
+    int status = 0;
+    sprintf(sql,"delete from issue where book_id = '%s' and member_id = '%s';", bk_id, m_id);
+    if(sqlite3_open(DB, &db) == SQLITE_OK)
+    {
+        if(sqlite3_exec(db, sql, 0, 0, &err_msg) == SQLITE_OK)
+            printf("SQL deletion successful\n");
+        else
+            status = printf("SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+    }
+    return status;
 }
 
